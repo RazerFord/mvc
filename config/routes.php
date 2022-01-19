2b38cdf1fa8671b7e6c16b1b6a2d22b3cb356ad8
RazerFord <desk10567@gmail.com>
2022-01-19 22:40:26 +0700

    begin mvc

---
+++ config/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'news/([0-9]+)' => 'news/view/$1/$2',
+    'news' => 'news/index',
+    'products' => 'product/list',
+];
---
+++ models/News.php
@@ -0,0 +1,49 @@
+<?php
+
+class News
+{
+    /**
+     * Returns single news item with specified id
+     * @param integer $id
+     */
+    public static function getNewsItemById($id)
+    {
+        $db = Db::getConnection();
+        $result = $db->query('SELECT id, title, date, short_content '
+            . 'FROM news '
+            . 'WHERE id = ' . $id
+        );
+
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+
+        $newsItem = $result->fetch();
+
+        return $newsItem;
+    }
+    /**
+     * Returns an array of news items
+     */
+    public static function getNewsList()
+    {
+        $db = Db::getConnection();
+        
+        $newsList = array();
+
+        $result = $db->query('SELECT id, title, date, short_content '
+            . 'FROM news '
+            . 'ORDER BY date DESC '
+            . 'LIMIT 10');
+
+        $i = 0;
+
+        while ($row = $result->fetch()) {
+            $newsList[$i]['id'] = $row['id'];
+            $newsList[$i]['title'] = $row['title'];
+            $newsList[$i]['date'] = $row['date'];
+            $newsList[$i]['short_content'] = $row['short_content'];
+            $i++;
+        }
+
+        return $newsList;
+    }
+}
